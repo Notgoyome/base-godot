a613c07dc6274a8d48b5949907948f2407216040
Guigui <113891088+Notgoyome@users.noreply.github.com>
2024-11-24 17:23:34 +0100

    reset

--- utils/check_point.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-@onready var marker : Marker2D = $Marker2D
-
-func _on_player_detector_player_detected(body:Player) -> void:
-	GameData.set_player_respawn_point(marker.global_position)
-	print("CHECKPOINT")
-	pass # Replace with function body.
